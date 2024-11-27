8c0cd2fc5707322173830bb130f7418d156434ab
tonipcv <xppsalvador@gmail.com>
2024-11-27 11:49:19 -0300

    com as chave na produção

--- app.config.js
+++ app.config.js
@@ -1,36 +1,21 @@
-module.exports = {
+import "dotenv/config";
+
+export default {
   expo: {
     name: "FIP",
-    slug: "FIP",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    slug: "fip",
+    version: "1.0.4", // Versão do app
     ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.yourcompany.fip"
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
-      package: "com.yourcompany.fip"
-    },
-    web: {
-      favicon: "./assets/favicon.png"
+      bundleIdentifier: "com.k17.ft", // Deve corresponder ao registrado no Apple Developer
+      buildNumber: "21",
+      icon: "./assets/icon.png", // Caminho para o ícone do iOS
     },
     extra: {
-      supabaseUrl: "https://hzqhyzwzrblcjdgjmash.supabase.co",
-      supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6cWh5end6cmJsY2pkZ2ptYXNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4NjE0NDAsImV4cCI6MjA0NTQzNzQ0MH0.T4Lt6Ci7Yro_SF6K4mXfavstyojL3BCfcDvRNJkSY3E"
-    }
-  }
-}; 
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL || "https://hzqhyzwzrblcjdgjmash.supabase.co",
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6cWh5end6cmJsY2pkZ2ptYXNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4NjE0NDAsImV4cCI6MjA0NTQzNzQ0MH0.T4Lt6Ci7Yro_SF6K4mXfavstyojL3BCfcDvRNJkSY3E",
+      eas: {
+        projectId: "8ac81449-a4df-454a-b543-b36cecc92b39", // Mantém o projectId do EAS
+      },
+    },
+  },
+};
